6e7be01addcb641ee99e16b4b65f0ed608f58bb5
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2022-04-19 12:07:14 -0300

    add page content

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="container">
+      <h1 className="title">CPF Search Engine</h1>
+      <div className="input-container">
+        <input type="text" placeholder="Enter your CPF..."></input>
+        <button className="btn-search">Search</button>
+      </div>
+      <div className="outcome">
+        <h3>CEP: 35460000</h3>
+        <p>Rua Ismael Neto</p>
+        <p>Bairro Silva Prado</p>
+        <p>Complemento: Apto 201</p>
+        <p>Brumadinho - MG</p>
+      </div>
     </div>
   )
 }
