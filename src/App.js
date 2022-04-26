0517c8acdb628d8dcb721105a5827115d3804d00
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2022-04-26 12:06:48 -0300

    add API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
 import { IoIosSearch } from 'react-icons/io'
 
 function App() {
+  const [input, setInput] = useState('')
+  const [dataAPI, setDataAPI] = useState([])
+
+  function handleInputChange(e) {
+    let inputData = e.target.value
+    setInput(inputData)
+  }
+
+  function handleSearch() {
+    if (input === '') {
+      alert('Preencha algum CEP!')
+    }
+
+    let url = `https://viacep.com.br/ws/${input}/json`
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setDataAPI(data)
+      })
+  }
+
   return (
     <div className="container">
       <h1 className="title">CPF Search Engine</h1>
       <div className="input-container">
-        <input type="text" placeholder="Enter your CPF..."></input>
-        <button className="btn-search">
+        <input
+          type="text"
+          placeholder="Enter your CPF..."
+          value={input}
+          onChange={handleInputChange}
+        ></input>
+        <button className="btn-search" onClick={handleSearch}>
           <IoIosSearch></IoIosSearch>
         </button>
       </div>
       <div className="outcome">
-        <h3>CEP: 35460000</h3>
-        <p>Rua Ismael Neto</p>
-        <p>Bairro Silva Prado</p>
-        <p>Complemento: Apto 201</p>
-        <p>Brumadinho - MG</p>
+        <h3>CEP: {dataAPI.cep}</h3>
+        <p>Rua {dataAPI.logradouro}</p>
+        <p>Bairro {dataAPI.bairro}</p>
+        <p>Complemento: {dataAPI.complemento}</p>
+        <p>
+          {dataAPI.localidade} - {dataAPI.uf}
+        </p>
       </div>
     </div>
   )
